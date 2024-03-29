2c592dd7d99f3cff420ff1de9275a724c9c8bde6
Thierry Jones <jones.thierry@gmail.com>
2024-03-28 20:12:00 -0400

    starter code for infinite slider

---
+++ pages/index.js
@@ -0,0 +1,165 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    const copyLogos = () => {
+      const logosSlide = document.querySelectorAll('.logos-slide');
+      logosSlide.forEach(slide => {
+        const copy = slide.cloneNode(true);
+        document.querySelector('.logos').appendChild(copy);
+      });
+    };
+    copyLogos();
+  }, []);
+  
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Starter App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <section>
+          <h1 className={styles.title}>
+            Welcome to <a href=''> TDOESDEV </a>JS animation tutotrial
+          </h1>
+        </section>
+
+        <section>
+          <p className={styles.description}>
+            I will show you how to animate some icons in <code>code</code>.
+          </p>
+        </section>
+
+        <section>
+          <div className={styles.logos}>
+            <div className={`${styles.slide} ${styles.slide1}`}>
+              {/* First set of logos */}
+              <img src="./logos/apple.svg" />
+              <img src="./logos/coca-cola.svg" />
+              <img src="./logos/ethereum.svg" />
+              <img src="./logos/facebook-messenger.svg" />
+              <img src="./logos/forbes.svg" />
+              <img src="./logos/mcdonald.svg" />
+              <img src="./logos/soundcloud.svg" />
+              <img src="./logos/spotify.svg" />
+              <img src="./logos/under-armour.svg" />
+              <img src="./logos/xbox-one.svg" />
+              <img src="./logos/apple.svg" />
+              <img src="./logos/coca-cola.svg" />
+              <img src="./logos/ethereum.svg" />
+              <img src="./logos/facebook-messenger.svg" />
+              <img src="./logos/forbes.svg" />
+            </div>
+            <div className={`${styles.slide} ${styles.slide2}`}>
+              {/* Second set of logos */}
+              <img src="./logos/apple.svg" />
+              <img src="./logos/coca-cola.svg" />
+              <img src="./logos/ethereum.svg" />
+              <img src="./logos/facebook-messenger.svg" />
+              <img src="./logos/forbes.svg" />
+              <img src="./logos/mcdonald.svg" />
+              <img src="./logos/soundcloud.svg" />
+              <img src="./logos/spotify.svg" />
+              <img src="./logos/under-armour.svg" />
+              <img src="./logos/xbox-one.svg" />
+              <img src="./logos/apple.svg" />
+              <img src="./logos/coca-cola.svg" />
+              <img src="./logos/ethereum.svg" />
+              <img src="./logos/facebook-messenger.svg" />
+              <img src="./logos/forbes.svg" />
+            </div>
+          </div>
+        </section>
+
+        
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+
+
+
+
